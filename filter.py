a71ba5908989537ac830eaad227e6af77b908046
Jameson Weng <weng.jameson126@gmail.com>
2016-09-17 12:58:54 -0400

    Scoring functions

--- filter.py
+++ filter.py
@@ -3,6 +3,8 @@ from googleapiclient import discovery
 import httplib2
 import json
 from oauth2client.client import GoogleCredentials
+import re
+import sys
 
 def getService():
     credentials = GoogleCredentials.get_application_default().create_scoped(
@@ -39,23 +41,85 @@ def analyzeSyntax(content, encodingType = 'UTF32'):
 		},
 		'features' : {
 			'extract_syntax' : True,
+			'extract_document_sentiment' : False,
+			'extract_entities' : False,
 		},
 		'encodingType' : encodingType,
 	}
 	return getService().documents().annotateText(body=body).execute()
 
-def complexSentence(response):
+def analyzeAll(content, encodingType = 'UTF32'):
+	body = {
+		'document' : {
+			'type' : 'PLAIN_TEXT',
+			'content' : content,
+		},
+		'features' : {
+			'extractSyntax' : True,
+			'extractDocumentSentiment' : True,
+			'extractEntities' : True,
+		},
+		'encodingType' : encodingType,
+	}
+	return getService().documents().annotateText(body=body).execute()
+
+def scoreEntities(response):
+	return len(response['entities'])
+
+def scoreSentiment(response):
+	polarity = response['documentSentiment']['polarity']
+	magnitude = response['documentSentiment']['magnitude']
+	return polarity * magnitude 
+
+def scoreComplexity(response):
 	numSentences = len(response['sentences'])
 
-def isQuestion(response):
-	questionMark = False
+	numSignificantTokens = 0
+
+	for token in response['tokens']:
+		significant = True
+
+		# ignore determiners and punctuation
+		if token['partOfSpeech']['tag'] in ['DET', 'PUNCT']:
+			significant = False
+
+		# ignore tokens that are just blabber (i.e. lololol)
+		else:
+			for regex in open("ignoredRegexes.txt").readlines():
+				if re.search(regex, token['text']['content']):
+					significant = False
+					break
+
+		if significant:
+			numSignificantTokens += 1
+
+	return numSentences * numSignificantTokens
+
+def scoreQuestion(response):
+	numQuestions = 0
 
 	for token in response['tokens']:
 		if token['text']['content'] == '?':
-			questionMark = True
-	return questionMark
+			numQuestions += 1
+	return numQuestions
 
+def isImportant(response):
+	entities_score = scoreEntities(response)
+	sentiment_score = scoreSentiment(response)
+	complexity_score = scoreComplexity(response)
+	question_score = scoreQuestion(response)
+	
+	print "response: ", response
+	print "entities score: ", entities_score
+	print "sentiment score: ", sentiment_score
+	print "complexity score: ", complexity_score
+	print "question score: ", question_score
 
 if __name__ == '__main__':
-	print(analyzeSyntax("where are we?"))
+	#response = analyzeAll("hello Statue of Liberty :)")
+	response = analyzeAll(sys.argv[1])
+
+	isImportant(response)
+
+	
 
